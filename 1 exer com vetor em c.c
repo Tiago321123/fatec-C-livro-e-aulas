08a0d7a702ec5b2e629671afd9026df73e79400c
Tiago <tiagodesouza12310@gmail.com>
2022-12-20 15:38:09 -0300

    vetor teste 10%

--- 1 exer com vetor em c.c
+++ 1 exer com vetor em c.c
@@ -7,19 +7,17 @@ int main()
         scanf("%d%*c",&numero[cont]);
     }
         
-        for(cont=0;cont<=7;cont++){
-            for (a=0;a<=numero[cont];a++) {
-
-        if (numero[cont] % a == 0){
-            i++;
-        
-        printf("%d\n",numero[cont]);
-        }
-        
+    for(cont=0;cont<=7;cont++){
+        for (a=0;a<=numero[cont];a++) {
+            if (numero[cont] % a == 0){
+                i++;
+            
+            printf("%d\n",numero[cont]);
+            }
         }
         if (i==2){
             printf("O número %d é primo, sua posicao é %d",numero[cont],cont);
         }
-        }
+    }
     return 0;
 }
--- teste ganho 10%.c
+++ teste ganho 10%.c
@@ -2,15 +2,16 @@
 int main(){
     float valor,soma=0,anos;
     int n;
-    printf("Digite o valor para somar 10%% dele: ");
+    printf("Digite o valor para somar 13%% dele: ");
     scanf("%f%*c",&valor);
-    printf("Digite a quantidade de anos: ");
+    printf("Digite a quantidade de anos: "); // ano tem 12 meses
     scanf("%f%*c",&anos);
-    for(n=0;n<anos;n++){
-    if(n==0){
-    soma= valor + valor*0.02;/* (valor*0.10)*vezes; */
-    }
-    else soma=soma + soma*0.02;
+
+    for(n=0;n<=anos;n++){
+        if(n==0){
+        soma= valor + valor*0.012;/* (valor*0.10)*vezes; */
+        }
+        else soma=soma + soma*0.012 + 150;
     }
     printf("Valor: %.2f\n",soma);
     printf("Valorizacao: %f",(soma/valor)*100);
